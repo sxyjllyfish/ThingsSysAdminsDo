d17de50fc1ee581aaba2783a4ac1f6a9b682d839
Sxyjllyfsh <moro@jjit.com.au>
2024-08-18 15:26:23 +1000

    Added in ADDS deployment Script

---
+++ ActiveDirectoryDomainServices/Configure-ADDS.ps1
@@ -0,0 +1,16 @@
+#Install the AD DS feature
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+Import-Module ADDSDeployment
+
+
+
+Install-ADDSForest `
+    -DomainName "sysadmin.local" `
+    -SafeModeAdministratorPassword (ConvertTo-SecureString "longFac351" -AsPlainText -Force) `
+    -InstallDns `
+    -DomainMode Win2012R2 `
+    -ForestMode Win2012R2 `
+    -LogPath "C:\ADDS\Logs"
+    -SysvolPath "C:\ADDS\SYSVOL"
+    -Force
